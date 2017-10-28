59a7fc04c52410cfc0c7a33771e417447fb0349d
Arkange <gabriel.noel@hotmail.fr>
2017-10-28 18:40:22 +0200

    Add Travis

--- App.cpp
+++ App.cpp
@@ -9,11 +9,12 @@
 
  int main()
  {
-	Product ballon("Ballon de Foot", "Ballon de foot aux couleurs du PSG", 501725, 24.99),
-	maillot("Maillot de foot", "Maillot de foot aux couleurs du PSG", 501726, 79.99),
-	bas("Short de foot", "Short de foot aux couleurs du PSG", 501727, 49.99),
-	casquette("Casquette PSG", "Casquette aux couleurs du PSG", 501728, 19.99),
-	echarpe("Echarpe PSG", "Echarpe aux couleurs du PSG", 501729, 14.99);
+	Product ballon("Ballon de Foot", "Ballon de foot aux couleurs du PSG", 501725, 24.99);
+	Product maillot("Maillot de foot", "Maillot de foot aux couleurs du PSG", 501726, 79.99);
+	Product bas("Short de foot", "Short de foot aux couleurs du PSG", 501727, 49.99);
+	Product casquette("Casquette PSG", "Casquette aux couleurs du PSG", 501728, 19.99);
+	Product echarpe("Echarpe PSG", "Echarpe aux couleurs du PSG", 501729, 14.99);
+	
 	Client client;
 	UI ui;
 	ui.askLastName();
